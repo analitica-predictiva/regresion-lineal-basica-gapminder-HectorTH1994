e8f11a2a3676349804cd378feb7108742272ad76
Hector David Taticuan <105394300+HectorTH1994@users.noreply.github.com>
2022-11-26 22:05:24 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -18,8 +18,8 @@ def pregunta_01():
     df = pd.read_csv('gm_2008_region.csv')
 
     # Asigne la columna "life" a `y` y la columna "fertility" a `X`
-    y = df.life
-    X = df.fertility
+    y = df[['life']]
+    X = df[['fertility']]
 
     # Imprima las dimensiones de `y`
     print(y.shape)
@@ -39,7 +39,7 @@ def pregunta_01():
     # Imprima las nuevas dimensiones de `X`
     print(X_reshaped.shape)
 
-
+pregunta_01()
 
 def pregunta_02():
     """
